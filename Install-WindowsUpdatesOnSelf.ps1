af0bf6dd2ed698d72f4bc485d2ef055b7a444ea6
LPRF <hello@liamf.net>
2018-04-16 15:52:22 +0100

    added self updater

---
+++ Install-WindowsUpdatesOnSelf.ps1
@@ -0,0 +1,16 @@
+﻿# Check for the WindowsUpdate module and if it doesnt exist - Install it. 
+if((Test-Path -Path 'C:\Program Files\WindowsPowerShell\Modules\WindowsUpdate\WindowsUpdate.psm1') -eq $null) { 
+
+    mkdir 'C:\Program Files\WindowsPowerShell\Modules\WindowsUpdate'
+
+    Invoke-WebRequest -Uri https://raw.githubusercontent.com/adbertram/Random-PowerShell-Work/master/Software%20Updates/WindowsUpdate.psm1 -OutFile 'C:\Program Files\WindowsPowerShell\Modules\WindowsUpdate\WindowsUpdate.psm1'
+
+}
+
+Import-Module WindowsUpdate
+
+Get-WindowsUpdate -ComputerName $env:COMPUTERNAME -Verbose
+
+Install-WindowsUpdate -ComputerName $env:COMPUTERNAME -Verbose
+
+Restart-Computer -Force
