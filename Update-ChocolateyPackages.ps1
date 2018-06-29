6fbcefc6f2732cc402841711a691bdd9c946d9ad
Sweggle <32654519+Sweggle@users.noreply.github.com>
2018-06-29 09:49:24 +0100

    added transcript output to C:\WSUSLogs

--- Update-ChocolateyPackages.ps1
+++ Update-ChocolateyPackages.ps1
@@ -1,1 +1,16 @@
-﻿choco upgrade all --yes
+﻿$VerbosePreference = 'Continue'
+# Set Log Location
+$LogPath = 'C:\WSUSLogs\'
+# Log Path Test
+if(!(Test-Path -Path $LogPath)) { mkdir $LogPath }
+# Set Log File Name
+$LogFileName = 'Chocolatey-Updates-' + (Get-Date -Format ddMMyy-HHmmss) + '.log'
+# Start Log Transcript
+$ErrorActionPreference = 'SilentlyContinue'
+Stop-Transcript | Out-Null
+$ErrorActionPreference = 'Stop'
+Start-Transcript -Path $($LogPath + $LogFileName) -Force
+# Upgrade all packages
+choco upgrade all --yes --verbose
+# Finished!
+Stop-Transcript | Out-Null
